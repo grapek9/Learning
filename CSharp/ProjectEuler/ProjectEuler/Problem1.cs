3fc4e0537dcb2a1aeebee53cf8221e7f709130d7
Grapker <zerex93@gmail.com>
2015-07-26 14:49:58 +0200

    First project euler program C#

---
+++ CSharp/ProjectEuler/ProjectEuler/Problem1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem1
+    {
+        public void run()
+        {
+            int sum = 0;
+            int[] array = Enumerable.Range(1, 999).ToArray();
+            foreach (int iterator in array)
+            {
+                if (iterator % 3 == 0 || iterator % 5 == 0)
+                {
+                    sum += iterator;
+                }
+            }
+            Console.WriteLine("Done and the sum equals : "+sum);
+        }
+    }
+}
---
+++ CSharp/ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1 run_Problem1 = new Problem1();
+            run_Problem1.run();
+            Console.ReadKey();
+        }
+    }
+}
