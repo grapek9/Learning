e3ba27c7a513995a84ae90354985064488ed07ea
Grapker <zerex93@gmail.com>
2015-07-28 22:59:52 +0200

    Solution for Problem2 (C#) at ProjectEuler2.com

---
+++ CSharp/ProjectEuler/ProjectEuler/Problem2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem2
+    {
+        public void run()
+        {
+            IEnumerable<int> list = generate_list();
+            display(list);
+        }
+        public IEnumerable<int> generate_list()
+        {
+            int firstArg = 1, secondArg = 1, temp = 0; 
+            do{
+                temp = secondArg;
+                secondArg = fibbonaciSequence(firstArg, secondArg);
+                firstArg = temp;
+                if(secondArg%2==0){
+                    yield return secondArg;
+                }
+            }while(secondArg<4000000);          
+        }
+        public int fibbonaciSequence(int firstArg,int secondArg)
+        {
+            return firstArg+secondArg;
+        }
+        public void display(IEnumerable<int> list)
+        {
+            int sum = 0;
+            foreach (int element in list)
+            {
+                sum += element;
+            }
+            Console.WriteLine("Sum of elements : "+sum);
+        }
+    }
+}
--- CSharp/ProjectEuler/ProjectEuler/Program.cs
+++ CSharp/ProjectEuler/ProjectEuler/Program.cs
@@ -10,8 +10,10 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Problem1 run_Problem1 = new Problem1();
-            run_Problem1.run();
+           // Problem1 run_Problem1 = new Problem1();
+           // run_Problem1.run();
+            Problem2 run_Problem2 = new Problem2();
+            run_Problem2.run();
             Console.ReadKey();
         }
     }
