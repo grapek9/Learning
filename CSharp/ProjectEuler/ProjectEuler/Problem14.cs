2f55400e4d0cb834fe4687893865e572f0dbbbbf
Grapker <zerex93@gmail.com>
2015-08-03 09:22:06 +0100

    Project Euler Problem14 (c#) minor bugs

--- CSharp/ProjectEuler/ProjectEuler/Problem14.cs
+++ CSharp/ProjectEuler/ProjectEuler/Problem14.cs
@@ -10,26 +10,63 @@ namespace ProjectEuler
     {
         public void run()
         {
-            int startingValue = 13;
+            IEnumerable<int> longestChain;
             
-            CollatzSequence(startingValue);
+            IEnumerable<int>[] listOfSequences = new IEnumerable<int>[1000000];
+            int value = 1;
+            int index = 0;
+            do
+            {
+                 listOfSequences[value] = CollatzSequence(value);
+                value++;
+                if (value % 10000 == 0) { Console.Clear(); Console.WriteLine("Processing: " + (value / 10000) + "%"); };
+            } while (value != 1000000);
+            longestChain = listOfSequences[1];
+            index = 1;
+            Console.WriteLine("Sequences generated, press key to proceed to searching for longest one");
+            Console.ReadKey();
+            for(int i = 2; i <= 999999; i++)
+            {
+                if (i % 10000 == 0) { Console.Clear(); Console.WriteLine("Processing: " + (i / 10000) + "%"); };
+                if (listOfSequences[i].Count() > listOfSequences[i - 1].Count())
+                {
+                    longestChain = listOfSequences[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("Founded, it begins with: "+index+" and has "+longestChain.Count()+" elements, keep pressing any key to see them all");
+            Console.ReadKey();
+            foreach(int element in longestChain)
+            {
+                Console.WriteLine(element+"->");
+                Console.ReadKey();
+            }
+
+            
+            
+            
         }
-        public void CollatzSequence(int number)
+        public IEnumerable<int> CollatzSequence(int number)
         {
-            Console.Write("Begins With-> 13 ->");
+            
+          
             do
             {
                 if (number % 2 == 0)
                 {
                     number /= 2;
-                    Console.Write(number+"->");
+                    
+                    yield return number;
                 }
                 else
                 {
                     number = 3 * number + 1;
-                    Console.Write(number + "->");
+                    
+                    yield return number;
                 }
+                
             } while (number != 1);
+
         }
     }
 }
