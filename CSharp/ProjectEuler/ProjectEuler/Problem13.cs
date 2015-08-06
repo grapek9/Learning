82af4e577c56a47ca603d7fd01aaace5bc2fc243
Grapker <zerex93@gmail.com>
2015-08-06 10:08:26 +0100

    Project Euler Problem13 (c#)

---
+++ CSharp/ProjectEuler/ProjectEuler/Problem13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    class Problem13
+    {
+        public void run()
+        {
+            BigInteger sum = 0;
+            System.IO.StreamReader readedValues = new System.IO.StreamReader("problem13values.txt");
+            for(int i = 0; i < 100; i++)
+            {
+                sum += BigInteger.Parse(readedValues.ReadLine());
+            }
+            Console.WriteLine(sum.ToString().Substring(0,10));
+
+        }
+    }
+}
