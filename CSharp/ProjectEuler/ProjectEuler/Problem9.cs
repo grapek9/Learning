beb5e79005acd0e1c9fbf425231c3244917826de
Grapker <zerex93@gmail.com>
2015-08-08 23:15:56 +0100

    Project Euler Problem 9 (c#) : minor changes , still to do

--- CSharp/ProjectEuler/ProjectEuler/Problem9.cs
+++ CSharp/ProjectEuler/ProjectEuler/Problem9.cs
@@ -10,18 +10,35 @@ namespace ProjectEuler
     {
         public void run()
         {
-            int a = 1, b = 2, c = 3;
+            int a = 3, b = 4,c= 5;
             do
             {
-                if((Math.Pow(a,2)+ Math.Pow(b, 2))== Math.Pow(c, 2))
+               // Console.WriteLine("test");
+                if (isTriplet(a, b))
                 {
-                    Console.WriteLine(a + " " + b + " " + c);
+                    c = (int)Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                    Console.WriteLine(a+" "+b+" "+c);
+                }
+                else
+                {
+                    do
+                    {
+                        b++;
+                        
+                    } while (isTriplet(a, b) ||b<300);
                     
                 }
                 a++;
-                b++;
-                c++;
+               // Console.WriteLine("=>>>>"+a+" "+b+" "+c);
             } while (a + b + c != 1000);
+        }
+        public bool isTriplet(int a,int b)
+        {
+            if (Math.Sqrt(Math.Pow(a,2)+Math.Pow(b,2))%1==0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
