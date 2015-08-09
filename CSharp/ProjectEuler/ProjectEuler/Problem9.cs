41e0013217357603a6793a17388d7295d9fc00c5
Grapker <zerex93@gmail.com>
2015-08-09 20:18:39 +0100

    Project Euler Problem 9 (c#) : done, trying to make own game in UNITY with C#

--- CSharp/ProjectEuler/ProjectEuler/Problem9.cs
+++ CSharp/ProjectEuler/ProjectEuler/Problem9.cs
@@ -10,35 +10,44 @@ namespace ProjectEuler
     {
         public void run()
         {
-            int a = 3, b = 4,c= 5;
-            do
+            IEnumerable<String> TripleSequences = generateTripleSequences();
+            foreach (String values in TripleSequences)
             {
-               // Console.WriteLine("test");
-                if (isTriplet(a, b))
-                {
-                    c = (int)Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-                    Console.WriteLine(a+" "+b+" "+c);
-                }
-                else
-                {
-                    do
-                    {
-                        b++;
-                        
-                    } while (isTriplet(a, b) ||b<300);
-                    
-                }
-                a++;
-               // Console.WriteLine("=>>>>"+a+" "+b+" "+c);
-            } while (a + b + c != 1000);
-        }
-        public bool isTriplet(int a,int b)
-        {
-            if (Math.Sqrt(Math.Pow(a,2)+Math.Pow(b,2))%1==0)
-            {
-                return true;
+                isTriplet(values.Split(','));
             }
-            return false;
+
+        }
+        public void isTriplet(String[] values)
+        {
+            int a = int.Parse(values[0]);
+            int b = int.Parse(values[1]);
+            int c = int.Parse(values[2]);
+            if ((a*a+b*b)==c*c)
+            {
+                if(a<b && b < c)
+                {
+                    Console.WriteLine(values[0]+"+"+values[1]+"+"+values[2]+"=1000");
+                    Console.WriteLine("Product: "+a*b*c);
+                }
+                
+            }
+            
+        }
+        public IEnumerable<String> generateTripleSequences()
+        {
+            for (int a = 1;a<=1000;a++)
+            {
+                for (int b = 1;b<=1000;b++)
+                {
+                    for (int c = 1;c<=1000;c++)
+                    {
+                        if ((a + b + c) == 1000)
+                        {
+                            yield return (a+","+b+","+c).ToString();
+                        }
+                    }
+                }
+            }
         }
     }
 }
