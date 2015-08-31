7a65907d0ffe2ae1cc5251bdd5c4a38cf5b49bad
Grapker <zerex93@gmail.com>
2015-08-31 10:11:18 +0100

    Project Euler Problem2 (Python)

--- Python/PythonLearning/PythonLearning/PythonLearning.py
+++ Python/PythonLearning/PythonLearning/PythonLearning.py
@@ -1,7 +1,13 @@
-﻿print("Project euler Problem1")
-list=list(range(1,1001))
-sum = 0 ;
-for i in range(len(list)):
-    if (i%3 == 0) | (i%5==0):
-        sum+= i   
+﻿print("Project Euler Problem2")
+argument1 = 1;
+argument2 = 1;
+temp = 0;
+sum = 0;
+while (argument2 <4000000):
+    temp = argument2
+    argument2 = argument1+argument2
+    argument1 = temp
+    if(argument2%2==0):
+        sum+=agrument2
+
 print(sum)
