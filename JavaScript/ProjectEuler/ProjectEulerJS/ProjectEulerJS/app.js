032bd40d137a7cb9060c3c39d939334c9e146126
Grapker <zerex93@gmail.com>
2015-09-14 18:06:47 +0200

    Project Euler Problem 4 (JavaScript)

--- JavaScript/ProjectEuler/ProjectEulerJS/ProjectEulerJS/app.js
+++ JavaScript/ProjectEuler/ProjectEulerJS/ProjectEulerJS/app.js
@@ -19,15 +19,15 @@ while (secondArg < 4000000) {
     (secondArg % 2 === 0) ? sum += secondArg : sum+=0;
 }
 console.log('Result ='+sum);*/
-console.log('Project Euler Problem 3');
+/*console.log('Project Euler Problem 3');
 function isPrime(value) {
     for (var i = 2; i < value; i++) {
         if (value % i === 0) {
             return false;
+            }
         }
-    }
     return true;
-}
+    }
 var value = 600851475143;
 var max = 0;
 var factor = 1;
@@ -37,8 +37,28 @@ while (value != 1) {
         if (isPrime(factor) === true) {
             max = factor;
             value = value / factor;
+            }
+        }
+}
+console.log('Result = '+max)*/
+console.log("Project Euler Problem 4");
+function isPalindrome(value) {
+    for (var i = 0; i < value.length; i++) {
+        if (value.charAt(i) != value.charAt(value.length - 1 - i)) {
+            return false;
+        }
+    }
+    return true;
+}
+var value = 0;
+for (var i = 100; i < 1000; i++) {
+    for (var j = 100; j < 1000; j++) {
+        if (isPalindrome((i * j).toString()) === true) {
+            if ((i * j) > value) {
+                value = i * j;
+            }
         }
     }
 }
-console.log('Result = ' + max);
+console.log('Result = ' + value);
 //# sourceMappingURL=app.js.map
