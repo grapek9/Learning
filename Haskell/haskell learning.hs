748da9103426dae6e9aa257d76fb3b05b4774cc5
grapek9 <zerex93@gmail.com>
2015-10-23 12:00:33 +0200

    project euler problem 1 haskell

--- Haskell/haskell learning.hs
+++ Haskell/haskell learning.hs
@@ -1,10 +1,10 @@
  
 
 rec 1 = 1
-
 rec x = x * rec (x-1)
-
+list = [1..999]
 
 main = do
-    print (rec 1000)
-
+    print (rec 1)
+    print list 
+    print (sum(filter (\n->n `mod` 3==0 || n`mod`5==0) list))
