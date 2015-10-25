1a6f0deea6053888879b7d99d94f2bd1f9b71dbe
grapek9 <zerex93@gmail.com>
2015-10-25 21:35:33 +0100

    project euler problem (haskell)

--- Haskell/haskell learning.hs
+++ Haskell/haskell learning.hs
@@ -1,10 +1,16 @@
  
 
-rec 1 = 1
-rec x = x * rec (x-1)
-list = [1..999]
-
+--rec 1 = 1
+--rec x = x * rec (x-1)
+--list = [1..999]
+factorial 1 = 1
+factorial x = x * factorial(x-1)
+--main = do
+--    print (rec 1)
+ --   print list 
+--    print (sum(filter (\n->n `mod` 3==0 || n`mod`5==0) list))
+n=10
+list = [1..n]
 main = do
-    print (rec 1)
-    print list 
-    print (sum(filter (\n->n `mod` 3==0 || n`mod`5==0) list))
+    print ((sum list)^^2-(sum(map(\n->n*n)list)))
+
