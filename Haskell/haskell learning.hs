bd8689aad1d51308874afc022dab2143570e0786
grapek9 <zerex93@gmail.com>
2015-10-26 21:36:56 +0100

    project euler problem (haskell)

--- Haskell/haskell learning.hs
+++ Haskell/haskell learning.hs
@@ -9,8 +9,15 @@ factorial x = x * factorial(x-1)
 --    print (rec 1)
  --   print list 
 --    print (sum(filter (\n->n `mod` 3==0 || n`mod`5==0) list))
-n=10
-list = [1..n]
-main = do
-    print ((sum list)^^2-(sum(map(\n->n*n)list)))
+--n=10
+--list = [1..n]
+--main = do
+--    print ((sum list)^^2-(sum(map(\n->n*n)list)))
 
+sideLength = 20
+fieldsize = 2*sideLength
+factorial 1 = 1
+factorial x = x * factorial(x-1)
+
+main = do
+	print ((factorial fieldsize)/((factorial sideLength)*(factorial (fieldsize-sideLength))))
